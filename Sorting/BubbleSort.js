a535e2837b072cc310a65025b728cf0a00430c37
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-01-22 13:11:19 -0600

    Completed bubble sort

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -1,5 +1,23 @@
+"use strict";
+// Bubble sort algorithm
+/**
+ *
+ * Simple and easy for small datasets
+ *
+ * worst case - O(n^2), Best case - O(n)
+ */
 const BubbleSort = function(inputArr) {
+  let loop = inputArr.length;
+  for (let i = 0; i < loop; i++) {
+    for (let j = 0; j < loop; j++) {
+      if (inputArr[j] > inputArr[j + 1]) {
+        let temp = inputArr[j];
+        inputArr[j] = inputArr[j + 1];
+        inputArr[j + 1] = temp;
+      }
+    }
+  }
   return inputArr;
 };
-// adding some test code
+
 module.exports = BubbleSort;
--- Sorting/BubbleSort.test.js
+++ Sorting/BubbleSort.test.js
@@ -2,6 +2,6 @@ const BubbleSort = require("./BubbleSort");
 
 describe("This test is to test bubble sort", () => {
   it("Positive test case for bubble sort", () => {
-    expect(BubbleSort([2, 4, 3, 1])).toBe([1, 2, 3, 4]);
+    expect(BubbleSort([2, 4, 3, 1])).toEqual([1, 2, 3, 4]);
   });
 });
