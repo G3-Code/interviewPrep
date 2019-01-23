5f246830fa0b10332f9fe67775e4e5a8a569215f
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-01-23 13:13:27 -0600

    Added one more test case for bubble sort

--- Sorting/BubbleSort.test.js
+++ Sorting/BubbleSort.test.js
@@ -4,4 +4,7 @@ describe("This test is to test bubble sort", () => {
   it("Positive test case for bubble sort", () => {
     expect(BubbleSort([2, 4, 3, 1])).toEqual([1, 2, 3, 4]);
   });
+  it("Positive test case for bubble sort", () => {
+    expect(BubbleSort([200, -4, 3, 10])).toEqual([-4, 3, 10, 200]);
+  });
 });
