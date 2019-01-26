ba7fcc597c23e35242b8f2477b163aa566ff7dd6
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-01-26 17:22:00 -0600

    Added Big O Example

--- BigOComplexity/Examples.js
+++ BigOComplexity/Examples.js
@@ -14,3 +14,14 @@ function example1(arr) {
 }
 
 //Ans: O(N)
+
+//Example 2
+function example2(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      console.log(arr[i] + "," + arr[j]);
+    }
+  }
+}
+
+//Ans: O(N^2)
