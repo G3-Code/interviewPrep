6d1d9e2f5b7c261cbcb11a6def9b8a4350127c1d
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-02-08 22:38:53 -0600

    Hashtable – remove function

--- InterviewQuestions/UniqueString.js
+++ InterviewQuestions/UniqueString.js
@@ -1,1 +1,11 @@
-module.exports = null;
+function UniqueString(value) {
+  let newStr = [...new Set(value.split(""))].toString().replace(/,/g, "");
+  if (newStr.length === value.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// UniqueString("crack");
+module.exports = UniqueString;
--- InterviewQuestions/UniqueString.test.js
+++ InterviewQuestions/UniqueString.test.js
@@ -4,4 +4,10 @@ describe("Test case for unique string", () => {
   it("should check for unique string", () => {
     expect(UniqueString("track")).toBe(true);
   });
+  it("should check for unique string", () => {
+    expect(UniqueString("crack")).toBe(false);
+  });
+  it("should check for unique string", () => {
+    expect(UniqueString("tempest")).toBe(false);
+  });
 });
