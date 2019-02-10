b36e9c3038bd9a553f9a86de7e9fde4536079549
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-02-10 08:25:54 -0600

    JS Interview prep

--- InterviewQuestions/JSQuestions.js
+++ InterviewQuestions/JSQuestions.js
@@ -10,7 +10,20 @@ function foo2() {
   }
 }
 
-console.log("foo1 returns");
-console.log(foo1());
-console.log("foo2 returns");
-console.log(foo2());
+// console.log("foo1 returns");
+// console.log(foo1());
+// console.log("foo2 returns");
+// console.log(foo2());
+
+function strRev(val) {
+  val = val.replace(/\W/g, "").toLowerCase();
+  return (
+    val ===
+    val
+      .split("")
+      .reverse()
+      .join("")
+  );
+}
+
+console.log(strRev("malayalam"));
